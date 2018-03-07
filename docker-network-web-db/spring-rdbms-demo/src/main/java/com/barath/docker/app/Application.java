3ec255937f7babe1a30b9b95da2ea2b27ea21337
Barath <barathmacdec91@gmail.com>
2018-03-07 22:40:06 +0530

    rdbms code

--- docker-network-web-db/spring-rdbms-demo/src/main/java/com/barath/docker/app/Application.java
+++ docker-network-web-db/spring-rdbms-demo/src/main/java/com/barath/docker/app/Application.java
@@ -3,109 +3,120 @@ package com.barath.docker.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+    UserService userService;
 
-	@RestController
-	public class UserController{
-
-		UserService userService;
-
-		public UserController(UserService userService) {
+	public Application(UserService userService) {
 			this.userService = userService;
-		}
+	}
 
-		@GetMapping("/users")
-		public List<User> getUsers(){
+	@GetMapping("/users")
+	public List<User> getUsers(){
 
-			return userService.getUsers();
-		}
-
+		return userService.getUsers();
 	}
 
 
-	protected class User{
-		private Long userId;
-		private String userName;
 
-		public Long getUserId() {
-			return userId;
-		}
 
-		public void setUserId(Long userId) {
-			this.userId = userId;
-		}
 
-		public String getUserName() {
-			return userName;
-		}
 
-		public void setUserName(String userName) {
-			this.userName = userName;
-		}
 
-		public User(Long userId, String userName) {
-			this.userId = userId;
-			this.userName = userName;
-		}
+
+
+}
+
+@Entity
+@Table(name="USER")
+class User{
+
+	@Id
+	@Column(name = "USER_ID")
+	private Long userId;
+	@Column(name = "USER_NAME")
+	private String userName;
+
+	public Long getUserId() {
+		return userId;
 	}
 
-	@Service
-	class UserService{
-
-		private UserRepository userRepository;
-
-
-		public UserService(UserRepository userRepository) {
-			this.userRepository = userRepository;
-		}
-
-		public User addUser(User user){
-
-			return userRepository.save(user);
-		}
-
-		public User getUser(String userName){
-			return userRepository.findByUserName(userName);
-		}
-
-		public List<User> getUsers(){
-			return userRepository.findAll();
-		}
-
-		@PostConstruct
-		public void init(){
-
-			User user1=new User(1L,"test1");
-			User user2=new User(1L,"test1");
-			Arrays.asList(user1,user2).forEach(this::addUser);
-
-		}
-
+	public void setUserId(Long userId) {
+		this.userId = userId;
 	}
 
-	interface UserRepository extends JpaRepository<User,Long>{
-
-		User findByUserName(String userName);
+	public String getUserName() {
+		return userName;
 	}
 
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public User(Long userId, String userName) {
+		this.userId = userId;
+		this.userName = userName;
+	}
+
+	public User() {
+	}
 }
 
 
 
+interface UserRepository extends JpaRepository<User,Long>{
+
+	User findByUserName(String userName);
+}
+
+@Service
+class UserService {
+
+	private UserRepository userRepository;
 
 
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
+	public User addUser(User user) {
 
+		return userRepository.save(user);
+	}
+
+	public User getUser(String userName) {
+		return userRepository.findByUserName(userName);
+	}
+
+	public List<User> getUsers() {
+		return userRepository.findAll();
+	}
+
+	@PostConstruct
+	public void init() {
+
+		User user1 = new User(1L, "test1");
+		User user2 = new User(1L, "test1");
+		Arrays.asList(user1, user2).forEach(this::addUser);
+
+	}
+
+}
